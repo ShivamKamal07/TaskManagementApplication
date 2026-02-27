16798427729c1ca2085b508169ab4b026b910c7e
kamal <shivamkamal.9936@gmail.com>
2026-02-27 22:55:45 +0530

    cookie issue solved

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -67,8 +67,8 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: none,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
