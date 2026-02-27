3bfe893f0c166ce407e584c95a614a95810f6d86
kamal <shivamkamal.9936@gmail.com>
2026-02-27 23:09:09 +0530

    cookie issue solved

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: none,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
