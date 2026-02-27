0c08f71a3d492ef8b0f08197f68f23b1b9c294a3
kamal <shivamkamal.9936@gmail.com>
2026-02-27 22:20:04 +0530

    appjs update

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ import { errorHandler } from "./middleware/error.middleware.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://task-management-application-livid.vercel.app", 
   credentials: true
 }));
 
